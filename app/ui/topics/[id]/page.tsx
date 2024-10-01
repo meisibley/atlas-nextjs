45a0479b17409ec73b5f4df1ba530056d03b8142
meisibley <mkong99@yahoo.com>
2024-09-30 21:55:52 -0500

    task2, 3 - setup database, data fetching

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { AskQuestion } from "@/components/AskQuestion";
+import { Question } from "@/components/Question";
+import { fetchQuestions, fetchTopic } from "@/lib/data";
+import { HashtagIcon } from "@heroicons/react/24/outline";
 
 export default async function Page({ params }: { params: { id: string } }) {
     // await new Promise((resolve) => setTimeout(resolve, 3000));
-    return <div>Topic Page: {params.id}</div>;
+    const topic = await fetchTopic(params.id);
+    const questions = await fetchQuestions(params.id);
+
+    if (!topic) {
+      return <div>Topic not found</div>;
+    }
+
+    return (
+      <div>
+        <h1 className="text-3xl font-black flex items-center">
+          <HashtagIcon className="h-6 w-6 mr-2" /> {topic.title}
+        </h1>
+        <AskQuestion topic={topic.id} />
+        {questions.map((question) => (
+          <Question
+            key={question.id}
+            id={question.id}
+            text={question.title}
+            votes={question.votes}
+          />
+        ))}
+        </div>
+    );
   }
   
